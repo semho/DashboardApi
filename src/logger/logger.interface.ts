b3e399feea7ff80ebef7d55917ce2adc7dadcf1a
unknown <semho@yandex.ru>
2022-12-20 06:16:28 +0300

    add nodemon

--- src/logger/logger.interface.ts
+++ src/logger/logger.interface.ts
@@ -1,8 +1,8 @@
-import { Logger } from "tslog";
+import { Logger } from 'tslog';
 
 export interface ILogger {
-  logger: Logger<ILogger>;
-  log: (...args: unknown[]) => void;
-  error: (...args: unknown[]) => void;
-  warn: (...args: unknown[]) => void;
+	logger: Logger<ILogger>;
+	log: (...args: unknown[]) => void;
+	error: (...args: unknown[]) => void;
+	warn: (...args: unknown[]) => void;
 }
--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -1,29 +1,29 @@
-import { injectable } from "inversify";
-import { Logger } from "tslog";
-import { ILogger } from "./logger.interface";
-import "reflect-metadata";
+import { injectable } from 'inversify';
+import { Logger } from 'tslog';
+import { ILogger } from './logger.interface';
+import 'reflect-metadata';
 
 @injectable()
 export class LoggerService implements ILogger {
-  public logger: Logger<LoggerService>;
+	public logger: Logger<LoggerService>;
 
-  constructor(name = "Logger") {
-    this.logger = new Logger({
-      name: name,
-      type: "pretty",
-    });
-  }
+	constructor(name = 'Logger') {
+		this.logger = new Logger({
+			name: name,
+			type: 'pretty',
+		});
+	}
 
-  log(...args: unknown[]) {
-    this.logger.info(...args);
-  }
+	log(...args: unknown[]): void {
+		this.logger.info(...args);
+	}
 
-  error(...args: unknown[]) {
-    //отправка в sentry/rollbar
-    this.logger.error(...args);
-  }
+	error(...args: unknown[]): void {
+		//отправка в sentry/rollbar
+		this.logger.error(...args);
+	}
 
-  warn(...args: unknown[]) {
-    this.logger.warn(...args);
-  }
+	warn(...args: unknown[]): void {
+		this.logger.warn(...args);
+	}
 }
